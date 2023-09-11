61b7be936172ff86efb862aa1688d1dab6e74192
dinesh1603 <j.dinesh1603@gmail.com>
2023-09-11 19:09:16 +0530

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+// import { DatabaseModule } from './database/database.module';
+import { UsersModule } from './modules/users/users.module'; 
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({isGlobal:true}),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: +configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    // DatabaseModule, 
+    UsersModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/modules/users/users.controller.ts
@@ -0,0 +1,44 @@
+import { Controller, Get, Post, Body, Put, Param, Delete, NotFoundException, HttpCode } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from './user.entity';
+
+@Controller('api/users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  async findAll(): Promise<User[]> {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  async getSingleUser (@Param('id') id: number, @Body() user: User): Promise<any> {
+    return this.usersService.findOne(id);
+    
+  }
+
+  @Post()
+  @HttpCode(201) 
+  async create(@Body() user: User): Promise<User> {
+    const createdUser = await this.usersService.create(user);
+    return createdUser;
+  }
+
+  @Put(':id')
+  async update (@Param('id') id: number, @Body() user: User): Promise<any> {
+    await this.usersService.update(id, user);
+    return { message: 'User updated successfully' };
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: number): Promise<any> {
+    const user = await this.usersService.findOne(id);
+
+    if (!user) {
+      throw new NotFoundException('User does not exist!');
+    }
+
+    await this.usersService.delete(id);
+    return { message: 'User deleted successfully' };
+  }
+}
